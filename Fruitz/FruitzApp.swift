9947eaf771fdd5d52246e849dd6ac84ec5e102d5
Andrei Kamarou <mr.akomarov@gmail.com>
2021-09-09 20:12:45 +0300

    Initial Commit

---
+++ Fruitz/FruitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitzApp.swift
+//  Fruitz
+//
+//  Created by Andrei Kamarou on 9.09.21.
+//
+
+import SwiftUI
+
+@main
+struct FruitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
